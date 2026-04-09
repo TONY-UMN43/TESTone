a067bc62d9f69418a9a8f492e4825da209197b5f
Dan Orban <dtorban@umn.edu>
2026-04-09 11:00:30 -0500

    Added basic unit test.

--- test/factorial_unittest.cpp
+++ test/factorial_unittest.cpp
@@ -1,2 +1,12 @@
 #include "fact.h"
 #include "gtest/gtest.h"
+
+TEST(FactorialTest, Demo) {
+  	//This is just a demo, replace this with better tests!
+	EXPECT_EQ(1, Factorial(0));
+}
+
+TEST(FactorialTest, DemoFail) {
+	//This test should fail!
+	EXPECT_EQ(10, Factorial(2));
+}
